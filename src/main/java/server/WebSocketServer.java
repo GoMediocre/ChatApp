c951e23067345d88c2f12b05c3049b8ba4a101ce
alex <jsinghalex0022@gmail.com>
2020-04-29 04:43:34 +0530

    find pair of user

--- src/main/java/server/WebSocketServer.java
+++ src/main/java/server/WebSocketServer.java
@@ -1,21 +1,18 @@
 package server;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ServerEndpoint("/action")
 public class WebSocketServer {
 
-      private static final Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
+      private static final Set<Session> users = Collections.synchronizedSet(new HashSet<Session>());
+      private static final Map<String, String> map = new HashMap<>();
 
       /** For reference DB Connection*/
       //https://www.oracle.com/webfolder/technetwork/tutorials/obe/java/HomeWebsocket/WebsocketHome.html#
@@ -26,11 +23,13 @@ public class WebSocketServer {
       public void onMessage(String message, Session session)
               throws IOException {
     	  System.out.println("Hellow World");
-            synchronized(clients){
+            synchronized(users){
                   // Iterate over the connected sessions
                   // and broadcast the received message
-                  for(Session client : clients){
+                  for(Session client : users){
                         if (!client.equals(session)){
+                              String id = session.getId();
+
                               client.getBasicRemote().sendText(message);
                         }
                   }
@@ -39,33 +38,50 @@ public class WebSocketServer {
 
       /** Single Client Only */
 //      @OnMessage
-//      public void onMessage(Session session,
+//      public void onMessage(Session user,
 //                            String message) {
 //            if(message.trim().isEmpty())
 //                  return;
 //            try {
-//                  session.getBasicRemote().sendText
+//                  user.getBasicRemote().sendText
 //                          ("Received message "+count+": " + message);
-//                  session.getBasicRemote().sendText
+//                  user.getBasicRemote().sendText
 //                          ("Lucky number! "+new Random().nextInt(99));
-//                  session.getBasicRemote().sendText
+//                  user.getBasicRemote().sendText
 //                          ("-----------------------------------");
 //            } catch (IOException ex) {
 //
 //            }
 //            count++;
 //      }
-      
+
       @OnOpen
-      public void onOpen (Session session) {
-            // Add session to the connected sessions set
-            clients.add(session);
+      public void onOpen (@PathParam("gender") String gender, Session user) {
+            // Add user to the connected sessions set
+            String id = user.getId();
+            user.getUserProperties().put("GENDER", gender);
+            user.getUserProperties().put("STATUS",  "NC");
+            users.add(user);
+            searchPair(user);
+      }
+
+      private void searchPair(Session currentUser) {
+
+            String currentUserGender = (String) currentUser.getUserProperties().get("GENDER");
+            String currentUserStatus = (String) currentUser.getUserProperties().get("STATUS");
+            for (Session user : users) {
+                  String gender = (String) user.getUserProperties().get("GENDER");
+                  String status = (String) user.getUserProperties().get("STATUS");
+                  if (!currentUserGender.equals(gender) && currentUserStatus.equals(status)) {
+                        //hear we find  two user :
+                  }
+            }
       }
 
       @OnClose
       public void onClose (Session session) {
             // Remove session from the connected sessions set
-            clients.remove(session);
+            users.remove(session);
       }
 
       @OnError
