a22981690ed5f778bbe77922b4a8663cd402962f
saravanakumar.rs <saravanakumar.rs@zohocorp.com>
2020-05-02 23:32:39 +0530

    Jersey basic authentication added

---
+++ src/main/java/auth/BasicAuthenticationHelper.java
@@ -0,0 +1,30 @@
+package auth;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.StringTokenizer;
+
+public class BasicAuthenticationHelper {
+    public boolean authenticate(String authCredentials) {
+
+        if (authCredentials == null) {
+            return false;
+        }
+
+        String encodedUserPassword = authCredentials.replaceFirst("Basic" + " ", "");
+        String usernameAndPassword = null;
+        try {
+            byte[] decodedBytes = Base64.getDecoder().decode(encodedUserPassword);
+            usernameAndPassword = new String(decodedBytes, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StringTokenizer tokenizer = new StringTokenizer(usernameAndPassword, ":");
+        String username = tokenizer.nextToken();
+        String password = tokenizer.nextToken();
+
+        boolean authenticationStatus = "admin".equals(username) && "admin".equals(password);
+        return authenticationStatus;
+    }
+}
