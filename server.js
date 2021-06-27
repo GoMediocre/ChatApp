a513b621093f3c2ce328ba2be1f8e9de0a96eac3
imrsk <imrsk@outlook.com>
2021-06-27 17:30:54 +0530

    The following fixes are added
    Queue the client on the disconnect of the other client
    
    Client getting mapped to self is fixed

--- server.js
+++ server.js
@@ -10,51 +10,68 @@ const port = process.env.PORT || 4000;
 const server = app.listen(port, () => console.log("Server started at :" +  port))
 
 client.on('connect', function() {
-    console.log('connected');
+    console.log('Connection established with Redis Server at port 6379');
 });
 
 const clientMap = new Map();
 const io = new Server(server);
 
-io.on('connection', (socket) => {
-    isClientExists((isExists) => {
-        if (isExists) {
-            mapClients(socket.id);
-        } else {
-            queueClient(socket.id);
-        }
-    });
+io.on('connection', async (socket) => {
+    await handleNewClientConnection(socket.id);
 
     socket.on('send', (data) => {
         const connectedWith = clientMap[socket.id];
         socket.to(connectedWith).emit("message", data);
     })
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', async () => {
+        console.log("Disconnecting socket : ", socket.id);
         const connectedWith = clientMap[socket.id];
-        console.log("Socket Id: " + socket.id + " is connected with" + connectedWith);
-        clientMap.delete(socket);
-        clientMap.delete(connectedWith);
-        io.to(connectedWith).emit("connectedStatus", false);
+        deleteFromQueue(socket.id);
+        if (connectedWith !== undefined) {
+            console.log("Socket Id: " + socket.id + " is connected with " + connectedWith);
+            clientMap.delete(socket);
+            clientMap.delete(connectedWith);
+            await handleNewClientConnection(connectedWith);
+        }
     })
 })
 
-function isClientExists(cb) {
-    client.scard('clients', (err, len) => {
-        console.log("Length:" + len);
-        if (err) {
-            console.log(err);
-            return false;
+async function handleNewClientConnection(socketId) {
+    try {
+        const isPending = await isAnyClientPendingToConnect();
+        console.log("isPending", isPending);
+        if (isPending) {
+            await mapClients(socketId);
+        } else {
+            await queueClient(socketId);
         }
-        cb(len > 0);
+    } catch (err) {
+        console.log("Error while connecting clients: ", err);
+        queueClient(socketId);
+    }
+}
+
+async function isAnyClientPendingToConnect() {
+    return new Promise((resolve, reject) => {
+        client.scard('clients', (err, len) => {
+            console.log("Clients pending to connect:" + len);
+            if (err) reject(err);
+            resolve(len > 0);
+        })
     })
 }
 
-function mapClients(socketId) {
+async function mapClients(socketId) {
+    console.log("Mapping client: ", socketId);
     client.spop('clients', (err, connectTo) => {
-        console.log("Popped " + connectTo);
         if (err) {
             console.log(err);
+            queueClient(socketId);
+            return;
+        }
+        console.log("Popped client: " + connectTo);
+        if (socketId === connectTo) {
             queueClient(socketId);
             return;
         }
@@ -66,10 +83,25 @@ function mapClients(socketId) {
     })
 }
 
-function queueClient(socketId) {
+async function queueClient(socketId) {
+    console.log("Queueing client: " + socketId);
     client.sadd('clients', socketId, (err, reply) => {
-        if (err) console.log(err);
+        if (err) {
+            console.log(err);
+            return;
+        }
         console.log("Pushed " + reply);
         io.to(socketId).emit('connectedStatus', false);
+    })
+}
+
+function deleteFromQueue(socketId) {
+    console.log("Removing client " + socketId);
+    client.srem('clients', socketId, (err, reply) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Removed client " + reply);
     })
 }
